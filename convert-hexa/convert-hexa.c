0ac932598824c9c76c27f6661c36d4f2543fe5e8
JuanJo Ciarlante <jjo@mendoza.gov.ar>
2006-03-17 15:42:15 -0300

    doc&beauty

--- convert-hexa/convert-hexa.c
+++ convert-hexa/convert-hexa.c
@@ -1,15 +1,20 @@
-/* 
- * convert-hexa: converts from hexa digits to buffer
- *
- * Author: Juanjo Ciarlante,  jjo (.) mendoza.gov.ar
+/** 
+ * \file convert-hexa.c converts from hexa digits to buffer
+ * \author Juanjo Ciarlante,  jjo (.) mendoza.gov.ar
  * License: GPLv2+
  */
 #include <stdio.h>
 #include <string.h>
 
-static char str_from_hexa[256];
+/** 256 char array for digit->value mapping */
+char map_hexa_to_value[256];
 
-static void init_str_from_hexa(char array[256]) {
+/** Initializes map_hexa_to_value array so that map_hexa_to_value[digit] yields
+ * its 4bit value, else -1
+ * \param array the 256 elem array to initialize
+ * \return void
+ */
+void init_map_hexa_to_value(char array[256]) {
 	int i;
 	memset(array, -1, 256);
 	for (i='0'; i<='9';i++) array[i]=i-'0'+0;
@@ -17,10 +22,22 @@ static void init_str_from_hexa(char array[256]) {
 	for (i='A'; i<='F';i++) array[i]=i-'A'+10;
 }
 
+/**
+ * Converts hexa 2 digits to bytes
+ * \param hexa 2-char array with hexa digits
+ * \return unsigned byte value or less than 0 if invalid hexa digits were passed
+ */
 static inline int hexa_to_byte(const unsigned char hexa[2]) {
-	return str_from_hexa[hexa[0]]*16+str_from_hexa[hexa[1]];
+	return map_hexa_to_value[hexa[0]]*16+map_hexa_to_value[hexa[1]];
 }
-static void *hexa_to_mem(unsigned char *buf, int size, const unsigned char *str, int len) {
+/**
+ * Converts a hexa string with len bytes to their value
+ * \param buf destination buffer
+ * \param size size of buf
+ * \param str hexa string
+ * \param len string len of str
+ */ 
+void *hexa_to_mem(unsigned char *buf, int size, const unsigned char *str, int len) {
 	int i, ret;
 	if (len%2) return NULL;
 	len/=2;
@@ -33,11 +50,14 @@ static void *hexa_to_mem(unsigned char *buf, int size, const unsigned char *str,
 	return buf;
 }
 
+/** Just a simple test, will convert str[] variable to its value and
+ * print it to stdout
+ */
 int main(void) {
 	unsigned char str[]="6a6a6f2052756c657a00";
 	unsigned char buf[256]="";
 	char *ret;
-	init_str_from_hexa(str_from_hexa);
+	init_map_hexa_to_value(map_hexa_to_value);
 	ret=hexa_to_mem(buf, sizeof(buf),str, strlen((char*)str));
 	printf("%s\n", ret? ret : "<NULL>");
 	return 0;
