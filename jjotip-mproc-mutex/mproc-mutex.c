cc761c976cc7aa87d95d77f29ca21b8b298d7501
JuanJo Ciarlante <jjo@mendoza.gov.ar>
2006-12-20 10:09:11 -0300

    minor readability touches

--- jjotip-mproc-mutex/mproc-mutex.c
+++ jjotip-mproc-mutex/mproc-mutex.c
@@ -6,9 +6,11 @@
  * Date: Wed Dec 20 09:26:29 ART 2006
 
  # Explanation:
-   pthread_mutex is a mutual exclusion lock usually used un multi-threaded applications.
-   This program shows a mutex usage that SURVIVES de timelife of a process (hence can be
-   used between processes) by making a shared-memory storage for the pthread_mutex_t object.
+   pthread_mutex is a mutual exclusion lock usually used un multi-threaded 
+   applications.
+   This program shows a mutex usage that SURVIVES de timelife of a process
+   (hence can be used between processes) by making a shared-memory storage 
+   for the pthread_mutex_t object.
    Tested on glibc-2.3+, linux-2.6 .
 
  # Compilation, just do: egrep make mproc-mutex.c | sh -x  ;-)
@@ -16,7 +18,7 @@
 
  # Usage:
 
- 	./mproc-mutex foo i   #initialize "foo" mutex, will appear as /dev/shm/foo
+ 	./mproc-mutex foo i   #initialize "foo" mutex, will be /dev/shm/foo
  	./mproc-mutex foo l   #lock; call it twice to see it "working"
  	./mproc-mutex foo u   #unlock 
 	rm /dev/shm/foo    #remove it
@@ -36,12 +38,14 @@ int main(int argc, char **argv)
 {
 	int fd;
 	pthread_mutex_t *p;
-	if (argc!=3) {fprintf(stderr, "usage: %s /mutex_file_name {i|l|u}\n", argv[0]);exit(EXIT_FAILURE);}
+	if (argc!=3)
+		{fprintf(stderr, "usage: %s /mutex_file_name {i|l|u}\n", argv[0]);exit(EXIT_FAILURE);}
 
 	ERR_EXIT( fd=shm_open(argv[1], O_RDWR|O_CREAT, 0666) );
 
 	if (*argv[2]=='i') 
-		ERR_EXIT( ftruncate(fd, sizeof(pthread_mutex_t)) ); /* expand the shm file to required size */
+		/* expand the shm file to required size */
+		ERR_EXIT( ftruncate(fd, sizeof(pthread_mutex_t)) ); 
 
 	/* map the shmfile to process memory */
 	ERR_EXIT ( p=mmap(NULL, sizeof(pthread_mutex_t), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0) );
