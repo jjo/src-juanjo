e13fedbe85dcdb43fb8b40d1578732c9544f6f6b
JuanJo Ciarlante <juanjosec@gmail.com>
2011-10-13 23:20:52 +0200

    implemented gen_grep, still missing argv handling

--- python/generators/gen-1.py
+++ python/generators/gen-1.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python2.5
+#!/usr/bin/python
 
 import os
 import fnmatch
 import gzip
 import bz2
+import re
 
 def gen_find(filepat,top):
   for path, dirlist, filelist in os.walk(top):
@@ -12,25 +13,32 @@ def gen_find(filepat,top):
 
 def gen_open(filenames):
   for fname in filenames:
-    if fname.endswith('.gz'):
-      yield(gzip.open(fname))
-    elif fname.endswith('.bz'):
-      yield(bz2.BZ2File(fname))
-    else:
-      yield(open(fname))
+    try:
+      if fname.endswith('.gz'):
+        yield(gzip.open(fname))
+      elif fname.endswith('.bz'):
+        yield(bz2.BZ2File(fname))
+      else:
+        yield(open(fname))
+    except IOError:
+      continue
 
 def gen_cat(files):
   for file in files:
     for line in file:
       yield(line)
 
-def gen_grep(lines):
-
+def gen_grep(lines, pattern):
+  regex = re.compile(pattern)
+  for line in lines:
+    if regex.match(line):
+      yield(line)
 
 filenames=gen_find('*.log*', '/var/log')
 files=gen_open(filenames)
 lines=gen_cat(files)
+patt=r'.*jjo\b'
+matches=gen_grep(lines, patt)
 
-
-for line in lines:
+for line in matches:
   print line,
