076db51cf6247f11cd615c43458350d6604de308
JuanJo Ciarlante <juanjosec@gmail.com>
2013-03-21 08:21:46 -0300

    support several input files, rearrange

--- juju/tools/juju-rel-matrix.py
+++ juju/tools/juju-rel-matrix.py
@@ -3,8 +3,8 @@
 ## Copyright 2013, Canonical Ltd.
 ## License: GPLv3
 ##
-# pylint: disable=C0103,W0142
 # vim: sw=4 ts=4 et ai
+# pylint: disable=C0103,W0142
 
 """
 Print a matrix relationnames, with servicenames by columns, rows
@@ -28,10 +28,45 @@ def get_common_prefix(words):
     common_chars = [c[0] for c in zip_words if c == (c[0],)*len(c)]
     return "".join(common_chars)
 
+def print_relations_matrix(juju_services, args):
+    "Print service-x-service relations matrix"
+    if args.full_name:
+        prefix = ''
+    else:
+        prefix = get_common_prefix(juju_services.keys())
+
+    if args.exclude:
+        exclude_re = re.compile('.*({}).*'.format(args.exclude))
+        juju_services = dict([x for x in juju_services.iteritems()
+                         if not exclude_re.match(x[0])])
+    if args.include:
+        include_re = re.compile('.*({}).*'.format(args.include))
+        juju_services = dict([x for x in juju_services.iteritems()
+                         if include_re.match(x[0])])
+
+    ## header: services by column
+    print '{}X{}'.format(prefix, args.separator), \
+          '{}'.format(args.separator).join(
+                sorted([x[len(prefix):] for x in juju_services.keys()]))
+    ## for each service, show the relation against service in column
+    for service_row in sorted(juju_services.keys()):
+        ## 1st column: service name
+        print "{}{}".format(service_row[len(prefix):], args.separator),
+        for _service_col, service_col_dict in sorted(juju_services.iteritems()):
+            rel_output = []
+            for rel_name in service_col_dict['relations']:
+                if service_row in service_col_dict['relations'][rel_name]:
+                    rel_output.append(rel_name)
+            if not rel_output:
+                rel_output.append('-')
+            print "{}{}".format(args.rel_separator.join(rel_output),
+                                args.separator),
+        print
+
 p = ArgumentParser(description='Juju relation matrix: %(prog)s filename'
                                '|column -t|less -SX',
                    epilog=__doc__, formatter_class=RawDescriptionHelpFormatter)
-p.add_argument('file', type=str, nargs=1,
+p.add_argument('files', type=str, nargs='+',
                help='file with "juju status" output, or "-" for stdin')
 p.add_argument('-x', '--exclude', type=str, action='store',
                help='exclude services that match this string regex, eg: '
@@ -44,52 +79,23 @@ p.add_argument('-r', '--rel-separator', type=str, action='store', default=',',
                help='same service pair relation separator (rare), default: ","')
 p.add_argument('-f', '--full-name', action='store_true',
                help='show full service names, vs stripping common prefix')
-args = p.parse_args()
+cmd_args = p.parse_args()
 
-if args.file[0] == '-':
-    input_file = sys.stdin
-else:
-    input_file = open(args.file[0])
+for filename in cmd_args.files:
+    if filename == '-':
+        input_file = sys.stdin
+    else:
+        input_file = open(filename)
 
-JUJU_STATUS = yaml.load(input_file)
-if not JUJU_STATUS:
-    print >> sys.stderr, "ERROR: stdin doesn't parse as YAML"
-    sys.exit(1)
+    juju_status = yaml.load(input_file)
+    if not juju_status:
+        print >> sys.stderr, "ERROR: {} doesn't parse as YAML", format(filename)
+        continue
 
-SERVICES = JUJU_STATUS.get('services', None)
-if not SERVICES:
-    print >> sys.stderr, "ERROR: no juju services found from {}".format(
-        args.file)
-    sys.exit(1)
-
-if args.full_name:
-    PREFIX = ''
-else:
-    PREFIX = get_common_prefix(SERVICES.keys())
-
-if args.exclude:
-    exclude_re = re.compile('.*({}).*'.format(args.exclude))
-    SERVICES = dict([x for x in SERVICES.iteritems() 
-                     if not exclude_re.match(x[0])])
-if args.include:
-    include_re = re.compile('.*({}).*'.format(args.include))
-    SERVICES = dict([x for x in SERVICES.iteritems() 
-                     if include_re.match(x[0])])
-
-## header: services by column
-print '{}X{}'.format(PREFIX, args.separator), '{}'.format(
-        args.separator).join(sorted([x[len(PREFIX):] for x in SERVICES.keys()]))
-## for each service, show the relation against service in column
-for service_row in sorted(SERVICES.keys()):
-    ## 1st column: service name
-    print "{}{}".format(service_row[len(PREFIX):], args.separator),
-    for service_col, service_col_dict in sorted(SERVICES.iteritems()):
-        rel_output = []
-        for rel_name in service_col_dict['relations']:
-            if service_row in service_col_dict['relations'][rel_name]:
-                rel_output.append(rel_name)
-        if not rel_output:
-            rel_output.append('-')
-        print "{}{}".format(args.rel_separator.join(rel_output), 
-                            args.separator),
-    print
+    services = juju_status.get('services', None)
+    if not services:
+        print >> sys.stderr, "ERROR: no juju services found from {}".format(
+            filename)
+        continue
+    print "##{}:".format(filename)
+    print_relations_matrix(services, cmd_args)
